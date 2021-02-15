1185ab79c5563a9bf5c2d26b775fbf2ccdbef5e9
kevin41307 <kevin41307@hotmail.com>
2021-02-15 18:32:45 +0800

    bind git

---
+++ Hello3/Assets/Scripts/Inventory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName ="New Inventory", menuName ="Inventory/New Inventory")]
+[System.Serializable]
+[SerializeField]
+public class Inventory : ScriptableObject
+{
+    public List<Item> itemList = new List<Item>();
+}
